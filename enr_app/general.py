6ae8a6b581b2660c77be2d1097592dbef2463c56
Bruno Lenzi <Bruno.Lenzi@cern.ch>
2022-07-05 10:54:44 +0200

    general: refactor load_zones, filtre sur départements et EPCIs dans select_zone, add docstring

--- enr_app/general.py
+++ enr_app/general.py
@@ -8,7 +8,20 @@ import geopandas as gpd
 
 @st.cache
 def load_zones():  # FIXME
-    return pd.read_csv('data/liste_zone_complete.csv', index_col=0)
+    regions = pd.read_json('https://geo.api.gouv.fr/regions').rename(columns={'nom': 'Zone', 'code': 'CodeZone'})
+    departements = pd.read_json('https://geo.api.gouv.fr/departements')\
+        .rename(columns={'nom': 'Zone', 'code': 'CodeZone'})\
+        .merge(regions.rename(columns={'Zone': 'Region', 'CodeZone': 'codeRegion'}), on='codeRegion')
+    epcis = pd.read_csv('data/liste_zones_dep.csv').astype({'dep': str}) \
+        .merge(departements.rename(columns={'Zone': 'Departement', 'CodeZone': 'dep'}), on='dep')
+    epcis = epcis[epcis['Zone'] != epcis['Departement']]
+    zones = pd.concat([regions, departements, epcis], keys=['Régions', 'Départements', 'Epci']) \
+        .reset_index().rename(columns={'level_0': 'TypeZone'})\
+        .drop(columns=['level_1', 'codeRegion', 'dep']) \
+        .sort_values(['TypeZone', 'Zone'])
+    return zones
+
+zones = load_zones()
 
 @st.cache
 def load_installations():
@@ -23,23 +36,15 @@ def load_indicateurs():
             index=['TypeZone', 'Zone', 'Filiere.de.production', 'annee'],
             values='valeur',
             columns='indicateur')
-    # Toutes -> Pays de la Loire, FIXME: remove
+
     indicateurs = pd.concat(
         [indicateurs.reset_index(),
-         indicateurs.reset_index().replace('Pays de la Loire', 'Toutes').query(
-             'TypeZone == "Régions" and Zone == "Toutes"')]
+         indicateurs.loc['Régions'].reset_index().groupby(
+             ['Filiere.de.production', 'annee']).sum().reset_index().assign(TypeZone='Régions', Zone='Toutes')
+         #      indicateurs.reset_index().replace('Pays de la Loire', 'Toutes').query('TypeZone == "Régions" and Zone == "Toutes"')
+         ]
     ).set_index(['TypeZone', 'Zone', 'Filiere.de.production', 'annee'])
-
     return indicateurs
-
-regions = pd.read_json('https://geo.api.gouv.fr/regions')
-departements = pd.read_json('https://geo.api.gouv.fr/departements')\
-    .merge(regions.rename(columns={'nom': 'region', 'code': 'codeRegion'}), on='codeRegion')
-epcis = pd.read_csv('data/liste_zones_dep.csv', index_col=0)\
-    .rename(columns={'Zone': 'nom', 'CodeZone': 'code'})
-    # .merge(departements.rename(columns={'nom': 'departement'}), left_on='dep', right_on='code')  # TODO: index as character
-zones = pd.concat([regions, departements, epcis], keys=['Régions', 'Départements', 'Epci'])\
-    .reset_index().rename(columns={'level_0': 'TypeZone', 'nom': 'Zone', 'code': 'CodeZone'})
 
 filieres = load_indicateurs().index.get_level_values('Filiere.de.production').unique().to_list()
 
@@ -48,16 +53,11 @@ region_default = 'Toutes'
 departement_default = 'Tous'
 epci_default = 'Tous'
 
-st.session_state['TypeZone'] = 'Régions'
-st.session_state['Zone'] = 'Pays de la Loire'
-
 st.session_state['region'] = region_default
 st.session_state['departement'] = departement_default
 st.session_state['EPCI'] = epci_default
 
-liste_regions = [region_default] + regions['nom'].sort_values().to_list()
-liste_departements = [departement_default] + departements['nom'].sort_values().to_list()
-liste_epcis = [epci_default] + zones.set_index('TypeZone').loc['Epci', 'Zone'].to_list()
+liste_regions = [region_default] + zones.set_index('TypeZone').loc['Régions', 'Zone'].to_list()
 
 st.session_state['filieres'] = filieres
 
@@ -65,15 +65,36 @@ def select_zone():
     """
     Sélectionne le territoire (région, département, EPCI) à travers 3 menus déroulants
 
-    TODO: sélectionner automatiquement région quand on choisi départment ?
-    TODO: changer departement/epci à default quand on choisi région/département
-
-    :return:
+    Returns: type zone (Régions, Départements ou Epci), zone (nom)
     """
-    st.session_state['region'] = st.sidebar.selectbox('Région', liste_regions)
-    # TODO: restreindre les départements et EPCIs à la région / département choisi
-    st.session_state['departement'] = st.sidebar.selectbox('Département', liste_departements)
-    st.session_state['EPCI'] = st.sidebar.selectbox('EPCI', liste_epcis)
+    def on_change_region():
+        st.session_state['departement'] = departement_default
+        st.session_state['EPCI'] = epci_default
+
+    def on_change_department():
+        st.session_state['EPCI'] = epci_default
+
+    st.session_state['region'] = st.sidebar.selectbox('Région', liste_regions,
+                                                      liste_regions.index(st.session_state['region']),
+                                                      on_change=on_change_region)
+
+    # Restreint les départements et EPCIs à la région / département choisi
+    reg = st.session_state['region'] if st.session_state['region'] != region_default else slice(None)
+    liste_departements = [departement_default] + \
+                         zones.set_index(['TypeZone', 'Region']).loc[('Départements', reg), 'Zone'].to_list()
+    st.session_state['departement'] = st.sidebar.selectbox('Département', liste_departements,
+                                                           liste_departements.index(st.session_state['departement']),
+                                                           on_change=on_change_department)
+
+    dep = st.session_state['departement'] if st.session_state['departement'] != departement_default else slice(None)
+    try:
+        liste_epcis = [epci_default] + zones.set_index(['TypeZone', 'Region', 'Departement'])\
+            .loc[('Epci', reg, dep), 'Zone']\
+            .drop_duplicates().to_list()
+    except:  # FIXME
+        liste_epcis = [epci_default] + zones.set_index('TypeZone').loc['Epci', 'Zone'].to_list()
+    st.session_state['EPCI'] = st.sidebar.selectbox('EPCI', liste_epcis,
+                                                    liste_epcis.index(st.session_state['EPCI']))
 
     if st.session_state['EPCI'] != epci_default:
         st.session_state['TypeZone'] = 'Epci'
@@ -90,36 +111,53 @@ def select_filieres():
     """
     Sélectionne les filières à considérer avec une checkbox par filière
 
-    :return: Un dictionnaire avec filière, True/False
+    Returns: un dictionnaire avec filière, True/False
     """
     st.sidebar.write('Filières')
     st.session_state['filieres'] = {fil: st.sidebar.checkbox(fil, True) for fil in filieres}
     return [k for k, v in st.session_state['filieres'].items() if v]
 
-def select_installations(type_zone=st.session_state['TypeZone'],
-                         zone=st.session_state['Zone'],
-                         filieres=None):
+def select_installations(type_zone, zone, filiere=None):
+    """
+    Sélectionne les installations
+
+    Args:
+        type_zone: str, (Régions, Départements ou Epci)
+        zone: str, nom de la zone
+        filiere: list (default: None)
+
+    Returns: DataFrame avec les installations sélectionnées
+    """
     installations = load_installations()
     column = {'Régions': 'NOM_REG',
               'Départements': 'NOM_DEP',
               'Epci': 'NOM_EPCI'}[type_zone]
     if type_zone != 'Régions' or zone != region_default:
         installations = installations.set_index(column).loc[zone].reset_index()
-    if filieres is not None:
+    if filiere is not None:
         selected_filieres = []
-        if 'Photovoltaïque' in filieres:
+        if 'Photovoltaïque' in filiere:
             selected_filieres.extend(['solaire photovoltaïque', 'solaire thermodynamique'])
-        if 'Eolien' in filieres:
+        if 'Eolien' in filiere:
             selected_filieres.extend(['éolien terrestre', 'éolien marin'])
-        if 'Bio Energie' in filieres:
+        if 'Bio Energie' in filiere:
             selected_filieres.extend(['méthanisation'])
         st.write(f'Filières sélectionnées: {selected_filieres}')
         return installations.loc[installations.typo.isin(selected_filieres)]
     return installations
 
-def select_indicateur(type_zone=st.session_state['TypeZone'],
-                      zone=st.session_state['Zone'],
-                      filiere=slice(None),
-                      annee=slice(None),
-                      indicateur=slice(None)):
+def select_indicateur(type_zone, zone, filiere=slice(None), annee=slice(None), indicateur=slice(None)):
+    """
+    Sélectionne un ou plusieurs indicateurs
+
+    Args:
+        type_zone: str, (Régions, Départements ou Epci)
+        zone: str, nom de la zone
+        filiere: liste ou valeur (default: toutes)
+        annee: liste ou valeur (default: tous)
+        indicateur: liste ou valeur (default: tous)
+
+    Returns: DataFrame avec le(s) indicateur(s) sélectionné(s)
+
+    """
     return load_indicateurs().loc[(type_zone, zone, filiere, annee), indicateur]
